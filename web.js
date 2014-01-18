846c300a04099deb69c56e5c184afb0fcdea1da9
tanakayuki <a0673040@gmail.com>
2014-01-18 12:06:23 +0900

    Add Procfile for Heroku

---
+++ web.js
@@ -0,0 +1,9 @@
+var app, express, gzippo;
+gzippo = require('gzippo');
+express = require('express');
+
+app = express();
+app.use(express.logger('dev'));
+app.use(gzippo.staticGzip("" + __dirname + "/dist"));
+app.listen(process.env.PORT || 5000);
+
